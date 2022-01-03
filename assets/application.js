50ac211d60018adbff974c0fe978e610425f63f2
raul-ditto <raulsamuelleos98@gmail.com>
2022-01-03 12:41:15 -0600

    article and blogs

---
+++ assets/application.js
@@ -0,0 +1,6 @@
+if (document.getElementById("sort_by") != null)
+  document.querySelector("#sort_by").addEventListener("change", function (e) {
+    let url = new URL(window.location.href);
+    url.searchParams.set("sort_by", e.currentTarget.value);
+    window.location = url.href;
+  });
